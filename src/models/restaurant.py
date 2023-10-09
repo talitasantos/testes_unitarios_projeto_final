fa9a3a43fe97e4d2e2fff35c5d67f42305ef4cc4
talitasantos2310@gmail.com <Maycon@1802>
2023-10-09 09:28:26 -0300

    Testes unitários do restaurant.py

--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -1,6 +1,7 @@
 class Restaurant:
     """Model de restaurante simples."""
 
+    #BUG: number_served e open nao foram passados como parametros no construtor
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name.title()
         self.cuisine_type = cuisine_type
@@ -9,18 +10,23 @@ class Restaurant:
 
     def describe_restaurant(self):
         """Imprima uma descrição simples da instância do restaurante."""
-        #
+        # BUG:
         #print(f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}.")
-        print(f"Esse restaturante chama {self.restaurant_name} and serve {self.cuisine_type}.")
-        print(f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto.")
+        # print(f"Esse restaturante chama {self.restaurant_name} e serve {self.cuisine_type}.")
+        # print(f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto.")
+
+        """Imprima uma descrição detalhada da instância do restaurante."""
+        description = f"Esse restaurante chama {self.restaurant_name} e serve {self.cuisine_type}\n Esse restaurante está servindo {self.number_served} consumidores desde que está aberto"
+        return description
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
         if not self.open:
-            # não é necessário definir self.open como False novamente, já que estamos abrindo o restaurante.
+            #BUG: não é necessário definir self.open como False novamente, já que estamos abrindo o restaurante.
             #self.open = False
             self.open = True
-            # para validar se o restaurante está aberto, número de serviço deve começa com 0
+            #BUG: para validar se o restaurante está aberto, número de serviço deve começa com 0
+            #self.number_served = -2
             self.number_served = 0
             return f"{self.restaurant_name} agora está aberto!"
         else:
@@ -40,11 +46,13 @@ class Restaurant:
         if self.open:
             self.number_served = total_customers
         else:
-            print(f"{self.restaurant_name} está fechado!")
+            return f"{self.restaurant_name} está fechado!"
 
     def increment_number_served(self, more_customers):
         """Aumenta número total de clientes atendidos por este restaurante."""
         if self.open:
-            self.number_served = more_customers
+            #BUG: nao estava incrementando
+            #self.number_served = more_customers
+            self.number_served += more_customers
         else:
-            print(f"{self.restaurant_name} está fechado!")
+            return f"{self.restaurant_name} está fechado!"
--- test/models/test_restaurant.py
+++ test/models/test_restaurant.py
@@ -1,16 +1,98 @@
+from src.models.restaurant import Restaurant
+
+
 class TestRestaurant:
 
     def test_describe_restaurant(self):
-        assert False
+        # Setup
+        restaurant = Restaurant('Lago Sul', 'churrasco')
+        restaurant.open_restaurant()
+        resposta_esperada = f"Esse restaurante chama Lago Sul e serve churrasco\n Esse restaurante está servindo 0 consumidores desde que está aberto"
+
+        # Chamada
+        resposta = restaurant.describe_restaurant()
+
+        # Avaliação
+        assert resposta_esperada == resposta
 
     def test_open_restaurant(self):
-        assert False
+        # Setup
+        restaurant = Restaurant('Lago Sul', 'churrascaria')
+        resposta_esperada = "Lago Sul agora está aberto!"
+
+        # Chamada
+        resposta = restaurant.open_restaurant()
+
+        # Avaliação
+        assert resposta_esperada == resposta
+
+        resposta_esperada = "Lago Sul já está aberto!"
+        # Chamada
+        resposta = restaurant.open_restaurant()
+
+        # Avaliação
+        assert resposta_esperada == resposta
 
     def test_close_restaurant(self):
-        assert False
+        # Setup
+        restaurant = Restaurant('Lago Sul', 'churrascaria')
+        resposta_esperada = "Lago Sul já está fechado!"
+
+        # Chamada
+        resposta = restaurant.close_restaurant()
+
+        # Avaliação
+        assert resposta_esperada == resposta
+
+        # Setup
+        resposta_esperada = "Lago Sul agora está fechado!"
+        restaurant.open_restaurant()
+
+        # Chamada
+        resposta = restaurant.close_restaurant()
+
+        # Avaliação
+        assert resposta_esperada == resposta
 
     def test_set_number_served(self):
-        assert False
+        # Setup
+        restaurant = Restaurant('Lago Sul', 'churrasco')
+        resposta_esperada = f"Lago Sul está fechado!"
+
+        # Chamada
+        resposta = restaurant.set_number_served(0)
+
+        # Avaliação
+        assert resposta_esperada == resposta
+
+        # Setup
+        restaurant.open_restaurant()
+        number_served_esperado = 2
+
+        # Chamada
+        restaurant.set_number_served(number_served_esperado)
+
+        # Avaliação
+        assert restaurant.number_served == number_served_esperado
 
     def test_increment_number_served(self):
-        assert False
+        # Setup
+        restaurant = Restaurant('Lago Sul', 'churrasco')
+        resposta_esperada = f"Lago Sul está fechado!"
+
+        # Chamada
+        resposta = restaurant.increment_number_served(2)
+
+        # Avaliação
+        assert resposta_esperada == resposta
+
+        # Setup
+        restaurant.open_restaurant()
+        restaurant.set_number_served(10)
+        number_served_esperado = 12
+
+        # Chamada
+        restaurant.increment_number_served(2)
+
+        # Avaliação
+        assert restaurant.number_served == number_served_esperado
